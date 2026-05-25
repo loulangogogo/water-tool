8051bee564585d4b790b6e368e0ea2065b9721a4
loulan <loulangogogo@163.com>
2026-05-25 18:16:59 +0800

    docs(exception): 添加异常类构造函数的文档注释
    - 为 CopyPropertieException 类的所有构造函数添加详细的 JavaDoc 注释
    - 为 DecryptException 类的所有构造函数添加详细的 JavaDoc 注释
    - 为 EncryptException 类的所有构造函数添加详细的 JavaDoc 注释
    - 添加参数说明和作者信息到所有新增的文档注释中

--- src/main/java/io/github/loulangogogo/water/exception/DecryptException.java
+++ src/main/java/io/github/loulangogogo/water/exception/DecryptException.java
@@ -7,22 +7,55 @@ package io.github.loulangogogo.water.exception;
  ** @since 8
  *********************************************************/
 public class DecryptException extends BaseException {
+    /**
+     * 构造一个无消息的解密异常
+     *
+     * @author :loulan
+     */
     public DecryptException() {
         super();
     }
 
+    /**
+     * 构造一个带消息的解密异常
+     *
+     * @param message 异常消息
+     * @author :loulan
+     */
     public DecryptException(String message) {
         super(message);
     }
 
+    /**
+     * 构造一个带消息和原因的解密异常
+     *
+     * @param message 异常消息
+     * @param cause   异常原因
+     * @author :loulan
+     */
     public DecryptException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * 构造一个带原因的解密异常
+     *
+     * @param cause 异常原因
+     * @author :loulan
+     */
     public DecryptException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * 构造一个带完整参数的解密异常
+     *
+     * @param message            异常消息
+     * @param cause              异常原因
+     * @param enableSuppression  是否启用抑制
+     * @param writableStackTrace 是否可写堆栈跟踪
+     * @author :loulan
+     */
     protected DecryptException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
--- src/main/java/io/github/loulangogogo/water/exception/EncryptException.java
+++ src/main/java/io/github/loulangogogo/water/exception/EncryptException.java
@@ -7,22 +7,55 @@ package io.github.loulangogogo.water.exception;
  ** @since 8
  *********************************************************/
 public class EncryptException extends BaseException {
+    /**
+     * 构造一个无消息的加密异常
+     *
+     * @author :loulan
+     */
     public EncryptException() {
         super();
     }
 
+    /**
+     * 构造一个带消息的加密异常
+     *
+     * @param message 异常消息
+     * @author :loulan
+     */
     public EncryptException(String message) {
         super(message);
     }
 
+    /**
+     * 构造一个带消息和原因的加密异常
+     *
+     * @param message 异常消息
+     * @param cause   异常原因
+     * @author :loulan
+     */
     public EncryptException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * 构造一个带原因的加密异常
+     *
+     * @param cause 异常原因
+     * @author :loulan
+     */
     public EncryptException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * 构造一个带完整参数的加密异常
+     *
+     * @param message            异常消息
+     * @param cause              异常原因
+     * @param enableSuppression  是否启用抑制
+     * @param writableStackTrace 是否可写堆栈跟踪
+     * @author :loulan
+     */
     protected EncryptException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
